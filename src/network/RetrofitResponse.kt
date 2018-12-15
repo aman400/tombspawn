d029e9a790c60f69bf086131b5d40a561edb4b86
Amandeep Anguralla <amandeep400@gmail.com>
2018-12-15 13:30:57 +0530

    Cleanup

--- src/network/RetrofitResponse.kt
+++ src/network/RetrofitResponse.kt
@@ -4,6 +4,6 @@ sealed class RetrofitResponse <out T>
 
 data class Success <out T> constructor(val data: T?): RetrofitResponse<T>()
 
-data class Failure <out T> constructor(val errorBody: String?, val throwable: Throwable? = null): RetrofitResponse<T>()
+data class Failure constructor(val errorBody: String?, val throwable: Throwable? = null): RetrofitResponse<Nothing>()
 
-data class CallError <out T> constructor(val throwable: Throwable?): RetrofitResponse<T>()
+data class CallError constructor(val throwable: Throwable?): RetrofitResponse<Nothing>()
